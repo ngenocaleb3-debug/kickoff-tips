ebf3547b8e798183752396f5b2192079f057ba69
ngenocaleb3-debug <ngenocaleb3@gmail.com>
2026-07-29 11:12:52 +0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.ngenocaleb3.kickofftips",
+  appName: "Kickoff Tips",
+  webDir: "dist",
+};
+
+export default config;
