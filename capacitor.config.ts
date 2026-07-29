32e0e6e1cd13a6b511bd215be2ed7c91e5d65903
ngenocaleb3-debug <ngenocaleb3@gmail.com>
2026-07-29 17:39:24 +0300

    Update and rename capacitor.config.ts to capacitor.config.json

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "com.ngenocaleb3.kickofftips",
-  appName: "Kickoff Tips",
-  webDir: "dist",
-};
-
-export default config;
